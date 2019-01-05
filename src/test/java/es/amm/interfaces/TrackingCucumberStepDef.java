194ecb5573ec0a0939e22048e70abb47e3b4ed4d
amartinm7 <amartinm7@gmail.com>
2019-01-05 18:06:22 +0100

    first commit, defining BDD tests

---
+++ src/test/java/es/amm/interfaces/TrackingCucumberStepDef.java
@@ -0,0 +1,109 @@
+package es.amm.interfaces;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import es.amm.domain.Event;
+import es.amm.domain.Shipment;
+import es.amm.domain.Tracking;
+import org.junit.Ignore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Ignore
+public class TrackingCucumberStepDef extends SpringBootBaseIntegrationTest {
+
+    private final Logger logger = LoggerFactory.getLogger(TrackingCucumberStepDef.class);
+
+    Shipment shipment;
+
+    Tracking tracking;
+
+    Event event;
+
+    public Shipment getShipment() throws IOException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Shipment shipment = objectMapper.readValue(this.getClass().getResourceAsStream("./shipment.json"), Shipment.class);
+        logger.info(objectMapper.writeValueAsString(shipment));
+        return shipment;
+    }
+
+    @Given("^the provided shipment$")
+    public void the_provided_shipment() throws IOException {
+        shipment = getShipment();
+        assertThat(!StringUtils.isEmpty(shipment.getReference())).isTrue();
+        tracking = new Tracking();
+    }
+
+    @When("^shipment reference should be equal to tracking (\\w+)$")
+    public void the_shipment_reference_should_be_equal_to_tracking_reference(final String trackingReference) {
+        tracking.setReference(trackingReference);
+        assertThat(shipment.getReference().equals(tracking.getReference()));
+    }
+
+    @When("^shipment parcel number should be equal to tracking parcel (\\w+)$")
+    public void shipment_parcel_number_should_be_equal_to_tracking_parcel_number(final Long trackingTotalParcels) {
+        tracking.setParcels(trackingTotalParcels);
+        assertThat(shipment.getParcels().size() == tracking.getParcels()).isTrue();
+    }
+
+    @When("^shipment total weight should be less than tracking weight (\\w+)$")
+    public void shipment_total_weight_should_be_less_than_tracking_weight(final Long trackingTotalWeight) {
+        tracking.setWeight(trackingTotalWeight);
+        assertThat(shipment.getTotalWeight() < tracking.getWeight()).isTrue();
+    }
+
+    @When("^shipment total weight should be greater or equal than tracking weight (\\w+)$")
+    public void shipment_total_weight_should_be_greater_or_equal_than_tracking_weight(final Long trackingTotalWeight) {
+        tracking.setWeight(trackingTotalWeight);
+        assertThat(shipment.getTotalWeight() >= tracking.getWeight()).isTrue();
+    }
+
+    @When("^tracking (\\w+) should be DELIVERED$")
+    public void tracking_status_should_be(final String trackingStatus) {
+        tracking.setStatus(Tracking.STATUS_TRACKING.valueOf(trackingStatus));
+        assertThat(tracking.getStatus().equals(Tracking.STATUS_TRACKING.DELIVERED)).isTrue();
+    }
+
+    @When("^tracking (\\w+) is not DELIVERED$")
+    public void tracking_status_is_not(final String trackingStatus) {
+        tracking.setStatus(Tracking.STATUS_TRACKING.valueOf(trackingStatus));
+        assertThat(!tracking.getStatus().equals(Tracking.STATUS_TRACKING.DELIVERED)).isTrue();
+    }
+
+    @When("^any other tracking field is null$")
+    public void any_other_tracking_field_is_null() {
+        boolean isAnyEmpty = tracking.getStatus() == null || tracking.getWeight() == null || tracking.getParcels() == null || StringUtils.isEmpty(tracking.getReference());
+        assertThat(isAnyEmpty).isTrue();
+    }
+
+    @When("^tracking (\\w+) is not equal to shipment reference$")
+    public void tracking_reference_is_not_equal_to_shipment_reference(final String trackingReference) {
+        tracking.setReference(trackingReference);
+        assertThat(!tracking.getReference().equals(shipment.getReference())).isTrue();
+    }
+
+    @Then("^dispatch an application event reference (\\w+) and status (\\w+)$")
+    public void dispatch_an_application_event(final String reference, final String status) {
+        event = new Event();
+        event.setReference(reference);
+        event.setStatus(status);
+        assertThat(event == event).isTrue();
+    }
+
+    @And("^print it into the console$")
+    public void print_it_into_the_console() {
+        assertThat(event == event).isTrue();
+        logger.info(event.toString());
+    }
+
+
+}
---
+++ src/test/resources/features/tracking.feature
@@ -0,0 +1,63 @@
+Feature: Tracking functionalities
+
+  Scenario Outline: tracking CONCILLIATION_REQUEST
+    Given the provided shipment
+    When shipment reference should be equal to tracking <trackingReference>
+    When shipment parcel number should be equal to tracking parcel <trackingParcelNumber>
+    When shipment total weight should be less than tracking weight <trackingTotalWeight>
+    When tracking <trackingStatus> should be DELIVERED
+    Then dispatch an application event reference <eventReference> and status <eventStatus>
+    And print it into the console
+
+  Examples:
+    | trackingReference | trackingParcelNumber  | trackingTotalWeight | trackingStatus  | eventReference  | eventStatus           |
+    | ABCD123456        | 2                     | 100                 | DELIVERED       | ABCD123456      | CONCILLIATION_REQUEST |
+
+
+  Scenario Outline: tracking NOT_NEEDED
+    Given the provided shipment
+    When shipment reference should be equal to tracking <trackingReference>
+    When shipment parcel number should be equal to tracking parcel <trackingParcelNumber>
+    When shipment total weight should be greater or equal than tracking weight <trackingTotalWeight>
+    When tracking <trackingStatus> should be DELIVERED
+    Then dispatch an application event reference <eventReference> and status <eventStatus>
+    And print it into the console
+
+  Examples:
+    | trackingReference | trackingParcelNumber  | trackingTotalWeight | trackingStatus  | eventReference  | eventStatus           |
+    | ABCD123456        | 2                     | 1                   | DELIVERED       | ABCD123456      | NOT_NEEDED |
+
+
+  Scenario Outline: tracking INCOMPLETE from DELIVERED
+    Given the provided shipment
+    When shipment reference should be equal to tracking <trackingReference>
+    When tracking <trackingStatus> is not DELIVERED
+    Then dispatch an application event reference <eventReference> and status <eventStatus>
+    And print it into the console
+
+  Examples:
+    | trackingReference | trackingStatus  | eventReference  | eventStatus |
+    | ABCD123456        | WAITING_IN_HUB  | ABCD123456      | INCOMPLETE  |
+
+
+  Scenario Outline: tracking INCOMPLETE FROM OTHER
+    Given the provided shipment
+    When shipment reference should be equal to tracking <trackingReference>
+    When any other tracking field is null
+    Then dispatch an application event reference <eventReference> and status <eventStatus>
+    And print it into the console
+
+    Examples:
+      | trackingReference | eventReference  | eventStatus |
+      | ABCD123456        | ABCD123456      | INCOMPLETE  |
+
+
+  Scenario Outline: tracking NOT_FOUND
+    Given the provided shipment
+    When tracking <trackingReference> is not equal to shipment reference
+    Then dispatch an application event reference <eventReference> and status <eventStatus>
+    And print it into the console
+
+    Examples:
+      | trackingReference | eventReference  | eventStatus |
+      | EFGH123456        | EFGH123456      | NOT_FOUND   |
