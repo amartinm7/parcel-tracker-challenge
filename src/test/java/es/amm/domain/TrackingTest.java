4d95d94a00fe3353cc27753167e38d895fb75326
amartinm7 <amartinm7@gmail.com>
2019-01-06 18:46:33 +0100

    testing the model

--- src/test/java/es/amm/domain/TrackingTest.java
+++ src/test/java/es/amm/domain/TrackingTest.java
@@ -1,9 +1,12 @@
 package es.amm.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TrackingTest {
 
@@ -14,5 +17,13 @@ public class TrackingTest {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Tracking tracking = objectMapper.readValue(this.getClass().getResourceAsStream("./tracking.json"), Tracking.class);
         logger.info(objectMapper.writeValueAsString(tracking));
+        Assert.assertTrue(tracking!=null);
+    }
+
+    @Test
+    public void createTrackingWrongStatus() throws Exception{
+        final ObjectMapper objectMapper = new ObjectMapper();
+        assertThrows(com.fasterxml.jackson.databind.exc.InvalidFormatException.class,
+                () -> objectMapper.readValue(this.getClass().getResourceAsStream("./trackingWrongStatus.json"), Tracking.class));
     }
 }
