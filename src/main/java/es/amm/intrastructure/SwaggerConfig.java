5f401db3f1357e0b455d50eb3e439dfd58eb5a2c
amartinm7 <amartinm7@gmail.com>
2019-01-06 18:44:22 +0100

    adding swagger to the application

---
+++ src/main/java/es/amm/intrastructure/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package es.amm.intrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("es.amm"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Parcel Tracker Challenge",
+                "Parcel Tracker Challenge Rest API",
+                "0.1.0",
+                "Terms of service",
+                new Contact("Antonio Martin Moreno", "https://github.com/amartinm7/parcel-tracker-challenge", "amartinm7@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
