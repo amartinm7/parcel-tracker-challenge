6019dc6fe72c86fb47ffdd77539eeb8d16dbd09e
amartinm7 <amartinm7@gmail.com>
2019-01-06 18:43:08 +0100

    applying validation status tracking

--- src/main/java/es/amm/domain/adapters/ShipmentCommandAdapter.java
+++ src/main/java/es/amm/domain/adapters/ShipmentCommandAdapter.java
@@ -1,13 +1,21 @@
 package es.amm.domain.adapters;
 
 
+import es.amm.domain.adapters.validations.ResolveTrackingStatus;
+import es.amm.intrastructure.Event;
 import es.amm.domain.Shipment;
+import es.amm.domain.Tracking;
 import es.amm.domain.ports.ShipmentCommandPort;
 import es.amm.intrastructure.repository.RepositoryPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class ShipmentCommandAdapter implements ShipmentCommandPort {
@@ -31,6 +39,19 @@ public class ShipmentCommandAdapter implements ShipmentCommandPort {
             throw new IllegalArgumentException(String.format("The Shipment reference %s already exists in the system.", shipment.getReference()));
         }
         return (storeShipment(shipment)) ? Optional.of(shipment) : Optional.empty();
+    }
+
+    @Override
+    public Optional<Event> addTracking (Tracking tracking){
+
+        Optional<Shipment> optionalShipment = repositoryPort.find(tracking.getReference());
+
+        final Optional<Event> event = ResolveTrackingStatus.getValidations().stream()
+                .map(resolveTrackingStatus -> resolveTrackingStatus.validate(optionalShipment,tracking))
+                .filter(optionalEvent -> optionalEvent.isPresent())
+                .findFirst().get();
+
+        return event;
     }
 
 }
