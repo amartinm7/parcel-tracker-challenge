e6d9b0d8d5df4f945d9c370877d230b98606a9a9
amartinm7 <amartinm7@gmail.com>
2019-01-06 18:43:51 +0100

    implementing controller

--- src/main/java/es/amm/interfaces/ShipmentController.java
+++ src/main/java/es/amm/interfaces/ShipmentController.java
@@ -3,12 +3,12 @@ package es.amm.interfaces;
 import es.amm.application.ShipmentService;
 import es.amm.domain.Shipment;
 import es.amm.domain.Tracking;
-import es.amm.intrastructure.CustomResponse;
+import es.amm.intrastructure.Event;
 import es.amm.intrastructure.HttpParams;
-import es.amm.intrastructure.ResponseMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,21 +27,24 @@ public class ShipmentController {
 
     @PostMapping(value = HttpParams.URI_REGISTER,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseMessage> register(@RequestBody Shipment shipment) {
+    public ResponseEntity<Shipment> register(@RequestBody Shipment shipment) {
         logger.info("asking for register shipment {}", shipment);
         return shipmentService.save(shipment)
-                .map(_shipment -> CustomResponse.getOKMessage(_shipment))
-                .orElse(CustomResponse.getInternalErrorMessage());
+                .map(_shipment -> new ResponseEntity(_shipment, HttpStatus.OK ))
+                .orElse(new ResponseEntity(shipment, HttpStatus.BAD_REQUEST));
     }
 
     @PutMapping(value = HttpParams.URI_PUSH,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseMessage> push(
+    public ResponseEntity<Event> push(
             @RequestBody Tracking tracking) {
         logger.info("asking for tracking {}", tracking);
         return shipmentService.addTracking(tracking)
-                .map(_tracking -> CustomResponse.getOKMessage(_tracking))
-                .orElse(CustomResponse.getInternalErrorMessage());
+                .map(event -> new ResponseEntity(event, HttpStatus.OK ))
+                .orElse( new ResponseEntity( new Event.Builder()
+                        .setStatus(Tracking.STATUS_TRACKING.NOT_FOUND)
+                        .setReference(tracking.getReference()).build(),
+                        HttpStatus.BAD_REQUEST));
     }
 
 }
