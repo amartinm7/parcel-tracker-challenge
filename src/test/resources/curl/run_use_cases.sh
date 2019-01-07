176f8b5fd75fa08bdbfe1c3eb99c4857b95cbf68
amartinm7 <amartinm7@gmail.com>
2019-01-07 11:26:34 +0100

    adding curl use cases for local testing

--- src/test/resources/curl/run_use_cases.sh
+++ src/test/resources/curl/run_use_cases.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
 curl -X POST "http://localhost:8085/api/register" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"reference\":\"ABCD123456\", \"parcels\" : [ { \"weight\":1, \"width\": 10, \"height\": 10, \"lenght\": 10 }, { \"weight\":2, \"width\": 20, \"height\": 20, \"lenght\": 20 } ]}"
-
-curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"parcels\": 1, \"reference\": \"string\", \"status\": \"DELIVERED\", \"weight\": 0}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"WAITING_IN_HUB\", \"parcels\":2, \"weight\":null, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"WAITING_IN_HUB\", \"parcels\":2, \"weight\":2, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"WAITING_IN_HUB\", \"parcels\":1, \"weight\":15, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"WAITING_IN_HUB\", \"parcels\":2, \"weight\":30, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"DELIVERED\", \"parcels\":2, \"weight\":2, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"DELIVERED\", \"parcels\":2, \"weight\":2, \"reference\":\"ABCD123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"DELIVERED\", \"parcels\":2, \"weight\":30, \"reference\":\"EFGH123456\"}"
+echo
+curl -X PUT "http://localhost:8085/api/push" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"status\":\"DELIVERED\", \"parcels\":null, \"weight\":30, \"reference\":\"ABCD123456\"}"
+echo
