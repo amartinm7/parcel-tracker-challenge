f0b67deed0614a4ce2b5092764d6687571123f93
amartinm7 <amartinm7@gmail.com>
2019-01-07 17:24:25 +0100

    cleaning code

--- src/main/java/es/amm/domain/adapters/validations/ResolveStatusTracking.java
+++ src/main/java/es/amm/domain/adapters/validations/ResolveStatusTracking.java
@@ -8,13 +8,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
-@FunctionalInterface
 public interface ResolveStatusTracking {
 
     Optional<Event> validate (final Optional<Shipment> optionalShipment, final Tracking tracking);
 
     static Collection<ResolveStatusTracking> getValidations (){
-        Collection<ResolveStatusTracking> collection = new ArrayList<>();
+        final Collection<ResolveStatusTracking> collection = new ArrayList<>();
         collection.add(new ConcilliationRequest());
         collection.add(new Incompleted());
         collection.add(new IncompletedByEmptyTrackingFields());
